ecad95c566fa7ba36c18020effecfd55c950239c
Alawi <alawidevelopment@gmail.com>
2024-02-21 02:47:44 +0700

    feat:upload to GCS

--- app/_components/HostUri.js
+++ app/_components/HostUri.js
@@ -1,8 +1,9 @@
 export const HostUri = 'https://ican.id/api/';
-export const HostImg = 'https://ican.id/assets/images/fotocorporate/';
+export const HostImg = 'https://storage.googleapis.com/ican-express/';
 export const HostRef = 'https://ican.id/registrasi/';
-// export const HostUri = 'http://192.168.0.110:8000/api/';
-// export const HostImgCorporate = 'http://192.168.0.110:8000/assets/images/fotocorporate/';
-// export const HostRef = 'http://192.168.0.110:8000/registrasi/';
+export const HostImgCorporate = 'https://storage.googleapis.com/ican-express/';
+// export const HostUri = 'http://192.168.0.115:8000/api/';
+// export const HostImg = 'http://192.168.0.115:8000/assets/images/fotocorporate/';
+// export const HostRef = 'http://192.168.0.115:8000/registrasi/';
 
 
